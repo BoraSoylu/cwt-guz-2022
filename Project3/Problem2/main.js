74bea0dc15e976b53feea058041169bcad234903
Bora Soylu <mail@borasoylu.com>
2022-11-28 17:30:22 +0300

    a

--- Project3/Problem2/main.js
+++ Project3/Problem2/main.js
@@ -1,7 +1,13 @@
-import generateAll from "./framework_at_home.js";
+import generateAll from './framework_at_home.js';
 
 fetch('./data.json')
   .then((response) => response.json())
   .then((json) => {
-    generateAll(json)
+    generateAll(json);
   });
+
+const sil = document.querySelector(
+  '.sil-bunu-gecici-github-pages-guncellendi-mi-diye-gormek-icin'
+);
+
+sil.innerText = '1';
