8a2a314a2ddd947d7c4a79f3c7cc353c123b9635
Bora Soylu <mail@borasoylu.com>
2022-12-20 17:28:49 +0300

    asd

--- Project5/src/components/App.js
+++ Project5/src/components/App.js
@@ -1,6 +1,8 @@
 import { addStudent, deleteStudentByObject, getAllStudents } from './FetchService.js';
 import { handlePerPageChange, generatePerPages } from './Pagination.js';
 import { studentRow } from './StudentRow.js';
+import Datepicker from 'flowbite-datepicker/Datepicker';
+import tr from '../../node_modules/flowbite-datepicker/js/i18n/locales/tr.js';
 
 export const App = () => {
   const elementClass = '.header-students';
@@ -31,8 +33,21 @@ export const App = () => {
         document.querySelectorAll('.dept-selection').forEach((z) => {
           z.value = b.value;
         });
-        
       });
     });
+    initDate();
   });
+  function initDate() {
+    Datepicker.locales.tr = tr.tr;
+
+    const datepickerOptions = {
+      language: 'tr',
+      weekStart: 1,
+    };
+    document.querySelectorAll('[datepicker]').forEach((datepickerEl) => {
+      const d = new Datepicker(datepickerEl);
+      d.setOptions(datepickerOptions);
+      console.log(d);
+    });
+  }
 };
